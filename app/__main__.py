9e52c503cdae6716e3a7068b3e15f17ef8d86942
ORobsonJr <robsonjrservices@gmail.com>
2022-09-27 18:47:05 -0300

    commit robson

--- app/__main__.py
+++ app/__main__.py
@@ -1,1 +1,25 @@
-#
+
+
+"""
+It's the setup file where user will run the whole program and your funcionalities
+"""
+
+from multiprocessing import Process
+from os import system
+
+def server():
+    from os import system
+
+    try:
+        system('cd api && uvicorn api:app --reload')
+        print('[*] Server started sucessfully')
+
+    except Exception as error_api:
+        print('A error trying run api server occurred: ', error_api)
+
+
+
+
+if __name__ == '__main__':
+    "Run the instances in paralell"
+    p1 = Process(target=server)
