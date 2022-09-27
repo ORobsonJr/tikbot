6ae6996a65aaba49c2d44243c93349b80ed72109
ORobsonJr <robsonjrservices@gmail.com>
2022-09-27 07:07:45 -0300

    Api comments

--- app/api/api.py
+++ app/api/api.py
@@ -8,10 +8,18 @@ class server():
 
     @app.get('/getAUTH')
     def getKeys(account: str = 'OPTIONAL'): #Use to choose a specific account, if any account be sent, return random 
+        """
+        Used to get api keys from youtube
+        """
         return 'API KEY to connection'
 
     @app.get('/getVideo')
     def getVideo(parameter: str = ''):
+        """
+        Used to get a video to upload, if no parameter is set, return a random video, otherwise
+         return a selected video and return a error
+        if these video doesn't exists
+        """
         if not parameter:
             return 'Random video'
 
