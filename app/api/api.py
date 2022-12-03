328b9e107f48db53642e51aafdf14670cddb3cb0
ORobsonJr <robsonjrservices@gmail.com>
2022-12-03 19:24:56 -0300

    Most important features released in api

--- app/api/api.py
+++ app/api/api.py
@@ -1,58 +1,81 @@
-from fastapi import FastAPI
-from sys import path
-path.append('../')
-from database.identify import IDENT
+from fastapi import FastAPI, HTTPException
 from os.path import dirname, abspath
 from random import choices
-from os import listdir
-
-
+from sys import path
+path.append('../../')
+from app.database.identify import IDENT
+from os.path import exists
+from app.vars.variable import LOCAL
+from uvicorn import run
 
 app = FastAPI()
+L = LOCAL()
+
 
 
 class server():
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.L = LOCAL() 
+        
 
     @app.get('/getAUTH')
     def getKeys(account_code: str): #Use to choose a specific account, if any account be sent, return random 
         """
-        Used to get api keys from youtube
+        Return parameters to login, in this case cookies.
         """
+        try:
+            account_code = account_code.replace(' ', '') #Sometimes we receive account_code with space, then let's replace it to avoid conflicts
 
-        return IDENT().return_data(identify_code=account_code)
+            file = IDENT(account_code).return_data()
+            if file:
+                arg = file['cookies']
 
+                if type(arg) == str: #Sometimes the file is stored in string format
+                    return eval(arg)
+
+                return file['cookies']
+            else:
+                raise HTTPException(status_code=404, detail='NOT FOUND')
+
+        except FileNotFoundError as e:
+            raise HTTPException(status_code=404, detail='NOT FOUND '+e)
+            
 
     @app.get('/getVideo') #Require a video
     def getVideo(video_file: str = ''):
         """
-        Used to get a video to upload, if no parameter is set, return a random video, otherwise
-         return a selected video and return a error
-        if these video doesn't exists
+        Used to get a video to upload, if no parameter is sended, return a random video.
+        Raise exception if these video doesn't exists.
         """
 
-
-        root_location = dirname(abspath("app")) #Get the root path
-        if video_file:
-            file_location = root_location + '/app/database/videos/' + video_file
-            return file_location
-
-        else:
-            file_location = root_location + '/../database/videos/'
-            videos = listdir(file_location)
-            return choices(videos) #Return a random video
-
-    @app.post('/postCookies')
-    def postCookies(cookies: dict):
-        #update cookies
-        return None
-            
+        try:
+            if video_file:
+                return L.return_video(video_file)
+            return L.return_video() #Random
+        except FileNotFoundError:
+            raise HTTPException(
+                status_code=404,
+                detail='EMPTY PATH'
+            )
 
 
+    @app.put('/putCookies')
+    def putCookies(cookies: dict, account_code: str):
+        """Update cookies of a specific account"""
+
+        try:
+            IDENT(account_code).update_data(value=cookies)
+
+            return 
+
+        except FileNotFoundError:
+            raise HTTPException(
+                status_code=400,
+                detail=f'FileNotFoundError',
+                )
+
+    
 
 
-
-
-if __name__ == '__main__':
-    server()
+def __main__():
+    run(app, host='localhost', port=8000)
