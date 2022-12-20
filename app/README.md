95c020f7c85f5e13a7b220634a199315a4bff60a
ORobsonJr <robsonjrservices@gmail.com>
2022-12-20 07:09:48 -0300

    README.md created

--- app/README.md
+++ app/README.md
@@ -1,32 +1,22 @@
-<h1>Documentação</h1>
+<h1>Documentation</h1>
+
+## Index
+* [Folders](#folders)
 
 
-<h3>/Api</h3><!--Robson-->
-<p>Irá realizar as conexões entre as features</p>
-<h4>Algumas integrações possíveis</h4>
-<ul>
-<li>Comunicação com Database</li>
-<li>Fazer requisição de chaves APIs</li>
-<li>Pegar conteúdo de um vídeo</li>
-<li>Verificar o status de algo em específico</li>
-<li>Comunicar comunicação com Database</li>
-</ul>
+## Folders
+`tiktok`
+Manage tiktok module.
 
-<h3>/Database</h3> <!--Robson -->
-<p>Vai cuidar do manuseio das contas, tais como chave api e hash. Também irá hospedar os vídeos baixados</p>
+`vars`
+Hold local variables, locations, arguments; in short it takes care of locations, local variables, manage of paths and directories.
 
-<h3>/Interface</h3> <!--Em processo...-->
-<p>[!]Assim que todas as features estiverem prontas, aguardar reunião para desenvolvimento da mesma.</p>
+`exceptions`
+Create and handle exceptions.
 
 
-<h3>/vars</h3> 
-<!-- Robson e Yago -->
-<!-- Em caso de qualquer alteração desse módulo comunicar com companheiro de equipe -->
-<ul>
-<li>Localizaçã de diretórios</li>
-<li>Chaves Apis</li>
-<li>Especificação de databases e localização</li>
-</ul>
+`database`
+Handle sqlite3 database.
 
-<h3>/youtube</h3> <!--Yago-->
-Responsável por controlar o youtube
+`api`
+Create api server and controll it.
--- start.py
+++ start.py
@@ -1,53 +1,129 @@
 """
-This file will run the __main__.py file
+Main file, file whose manage everything
 """
+
 from os import system
 from sys import version_info 
-from app.youtube.getAPI import handleAPI
+from multiprocessing import Process
+from app.vars.variable import LOCAL
+from app.api import api
 
-class setup():
-    reQfile = 'requirements.txt'
 
-    def __init__(self):
-        self.python_version = version_info[0]
-        
+reQfile = 'requirements.txt'
 
-    def install(self):
-        system('pip{py_v} install -r requirements.txt'.format(py_v=self.python_version)) #Install the requirements
+python_version = version_info[0] #Python version
+    
+def install():
+    """Install requirements.txt using the current version of python"""
+    system('pip{py_v} install -r {file}'.format(py_v=python_version, file=reQfile)) #Install the requirements
+    system(f'python{python_version} setup.py install')
 
-        #Used to install prerequisites
+def setting():
+    """Create a file with location of webriver"""
+    from app.vars.variable import LOCAL
 
-    def server(self):
-        system('cd app/api && uvicorn api:app --reload')
+    LOCAL().create_webdriver_location()
 
-    def youtube(self):
-        handleAPI()
 
+def server():
+    """Run api server"""
+    api.__main__()
+
+
+def tiktok(account_id: str, video: str = ''):
+    """Upload video"""
+    from app.tiktok.start import TIKTOK as TK
+
+    if video:
+        TK().__main__(account_id, video)
+    else:
+        TK().__main__(account_id)
+
+
+            
 
 if __name__ == '__main__':
     from sys import argv
+    from app.database.identify import IDENT
 
-    SET = setup()
 
-    try:
-        if argv[1] ==('-i' or '--install'):
-            SET.install()
-        
-
-        if argv[1] ==('-s' or '--server'):
-            SET.server()
-
-        if argv[1] ==('-y' or '--youtube'):
-            SET.youtube()
-        
-    except IndexError:
-        print("""
+    help_str = """
         Usage python3 start.py [option]
         
+        -h or --help       Return commands
+
+
         -i or --install:    Install dependencies and modules required to project, it's
          usually is used to the first use
 
          -s or --server:    Run only the api server
 
+         -t or --tiktok [account_id]:    Run exclusively tiktok module
+
+         -c or --configure:    Configure settings
+
+         -u or --users:    Show all users 
+
+         run [account code]               Run the entire program
+
+
         """
-        )
+        
+
+    L = LOCAL()
+
+    try:
+        arg = argv[1]
+
+        if arg ==('-i' or '--install'):
+            print('Installing...')
+            install()
+        
+
+        elif arg ==('-s' or '--server'):
+            print('Running exclusively api server...')
+            server()
+
+        elif arg ==('-t' or '--tiktok'):
+            print('Running exclusively tiktok module')
+            tiktok(argv[2])
+
+        elif arg == ('-h' or '--help'):
+            print(help_str)
+
+        elif arg == ('-c' or '--configure'):            
+            setting()
+
+        elif arg == ('-u' or '--users'):
+            try:
+                account_argument = argv[2]
+                LOCAL(account_argument).return_users()
+            except IndexError:
+                LOCAL().return_users()
+
+        elif argv[1] == 'run':
+            """Run the entire code, server and tiktok"""
+            print('Running Tikbot software...')
+
+            user_status = IDENT(account_id=argv[2]).checkUser()
+
+            
+            if user_status == True:
+            
+                p2 = Process(target=tiktok, args=(argv[2]))
+                p2.start()
+
+                p1 = Process(target=server()) #Run server in paralell
+                p1.start()
+
+                p1.join()
+                p2.join()
+            else:
+                print("Please provide a existent user id, to see all users type python3 start.py -u")
+
+        else:
+            print(help_str)
+        
+    except IndexError:
+        print(help_str)
+
